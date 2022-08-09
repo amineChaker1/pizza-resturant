fc17f00354d568e9c70b7f98c37dbe8fe68d70d9
Chaker Amine <chkramine2003@gmail.com>
2022-08-09 16:18:40 +0200

    main page done

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import "@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js";
+import "@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css";
+import "jquery/dist/jquery.min.js";
+import "@popperjs/core/dist/cjs/popper.js";
+import "../sass/style.scss";
+console.log("hello world");
