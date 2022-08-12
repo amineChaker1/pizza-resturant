19d53c5cdbeb17862da7a054361f14a5018987c6
Chaker Amine <chkramine2003@gmail.com>
2022-08-12 02:12:58 +0200

    final touches left

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,30 @@ import "jquery/dist/jquery.min.js";
 import "@popperjs/core/dist/cjs/popper.js";
 import "../sass/style.scss";
 console.log("hello world");
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName("needs-validation");
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
